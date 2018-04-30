d1a4079c0fe5b8fc03f9bdb593abd876e7229b93
xxxgitone <xxxgit@sina.com>
2018-04-30 23:29:29 +0800

    commonsChunkPlugin

--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -1,10 +1,17 @@
+const webpack = require('webpack')
+const path = require('path')
+
 module.exports = {
   entry: {
-    app: './src/index.ts'
+    pageA: './src/pageA.js',
+    pageB: './src/pageB.js',
+    vendor: ['lodash']
   },
 
   output: {
-    filename: '[name].[hash:8].js'
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js'
   },
 
   module: {
@@ -23,5 +30,33 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+
+  plugins: [
+    // 提取指定文件的公共部分
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'common',
+    // 重复两次
+    //   minChunks: 2,
+    //   chunks: ['pageA', 'pageB']
+    // }),
+
+    // 打包第三方的库
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity
+    }),
+
+    // 项目的业务代码
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      minChunks: Infinity
+    })
+
+    // 简写
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   names: ['vendor', 'manifest'],
+    //   minChunks: Infinity
+    // })
+  ]
 }
